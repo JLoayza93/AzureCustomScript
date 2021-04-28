e8e18d8120867f0639799efb850e5d2ab726aca7
JLoayza93 <40008461+JLoayza93@users.noreply.github.com>
2021-04-28 17:09:59 -0400

    Update chocolateywinscp.ps1

--- chocolateywinscp.ps1
+++ chocolateywinscp.ps1
@@ -1,3 +1,2 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install winscp -y
